165964ef8a37584fc396149f9b9fe1192e03a1ce
nessie <nessie@hotmail.dk>
2024-11-02 19:56:18 +0100

    One Weird Trick to hide white lines/dots in models.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinTextureAtlasSprite.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinTextureAtlasSprite.java
@@ -2,13 +2,23 @@ package net.dugged.nessie.himmelbjerget.mixins;
 
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TextureAtlasSprite.class)
 public abstract class MixinTextureAtlasSprite {
 	@ModifyConstant(method = "initSprite", constant = @Constant(doubleValue = (double) 0.01F))
 	private double himmelbjerget$noZoom(final double constant) {
 		return 0F;
+	}
+
+	@Inject(method = {"getInterpolatedU", "getInterpolatedV"}, at = @At("RETURN"), cancellable = true)
+	private void himmelbjerget$vroom(final double uv, final CallbackInfoReturnable<Float> cir) {
+		final float value = cir.getReturnValue();
+		final float factor = 1F / 16384F;
+		cir.setReturnValue(Math.round(value / factor) * factor);
 	}
 }

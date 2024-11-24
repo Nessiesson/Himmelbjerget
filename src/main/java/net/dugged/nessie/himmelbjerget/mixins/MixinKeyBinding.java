7ea5607e789c8a57f89ccee0b989523cabbad91e
nessie <nessie@hotmail.dk>
2024-11-24 20:59:33 +0100

    Make variable final.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinKeyBinding.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinKeyBinding.java
@@ -14,7 +14,7 @@ public abstract class MixinKeyBinding {
 	// TODO: Figure out why this requires gg.essential.lib.mixinextras.sugar.Local and not com.llamalad7.mixinextras.sugar.Local; 💀💀💀
 	@Inject(method = "setKeyBindState", at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/KeyBinding;pressed:Z", opcode = Opcodes.PUTFIELD))
 	private static void himmelbjerget$updateKeyState(final int keyCode, final boolean pressed, final CallbackInfo ci, final @Local KeyBinding keyBinding) {
-		if (keyBinding instanceof ToggleSettingKeyBinding toggleSetting && !((IKeyBinding) keyBinding).getPressed() && pressed) {
+		if (keyBinding instanceof final ToggleSettingKeyBinding toggleSetting && !((IKeyBinding) keyBinding).getPressed() && pressed) {
 			toggleSetting.toggle();
 		}
 	}

aa2ec95ffa8357a8efdcad199f6a18f601aada5f
nessie <nessie@hotmail.dk>
2024-12-04 05:26:20 +0100

    There are still big explosions happening somewhere. Add logging.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinNetHandlerPlayClient.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinNetHandlerPlayClient.java
@@ -1,5 +1,6 @@
 package net.dugged.nessie.himmelbjerget.mixins;
 
+import net.dugged.nessie.himmelbjerget.Himmelbjerget;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.play.server.S2APacketParticles;
 import net.minecraft.util.EnumParticleTypes;
@@ -12,8 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinNetHandlerPlayClient {
 	@Inject(method = "handleParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketThreadUtil;checkThreadAndEnqueue(Lnet/minecraft/network/Packet;Lnet/minecraft/network/INetHandler;Lnet/minecraft/util/IThreadListener;)V", shift = At.Shift.AFTER))
 	private void himmelbjerget$onParticle(final S2APacketParticles p, final CallbackInfo ci) {
-		if (p.getParticleType() == EnumParticleTypes.EXPLOSION_LARGE && p.isLongDistance() && p.getParticleCount() == 8) {
-			((IS2APacketParticles) p).setParticleType(EnumParticleTypes.VILLAGER_HAPPY);
+		if (p.getParticleType() == EnumParticleTypes.EXPLOSION_LARGE) {
+			if (p.isLongDistance() && p.getParticleCount() == 8) {
+				((IS2APacketParticles) p).setParticleType(EnumParticleTypes.VILLAGER_HAPPY);
+			} else {
+				Himmelbjerget.LOGGER.info("{} {} {}", p.isLongDistance(), p.getParticleSpeed(), p.getParticleCount());
+			}
 		}
 	}
 }

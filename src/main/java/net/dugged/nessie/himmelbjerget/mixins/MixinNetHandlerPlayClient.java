a319940391c31b15573d0520e0b618dd0d3bddb7
nessie <nessie@hotmail.dk>
2024-12-05 09:52:41 +0100

    Simply replace all large explosion particles with happy villager particles.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinNetHandlerPlayClient.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinNetHandlerPlayClient.java
@@ -1,6 +1,5 @@
 package net.dugged.nessie.himmelbjerget.mixins;
 
-import net.dugged.nessie.himmelbjerget.Himmelbjerget;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.network.play.server.S2APacketParticles;
 import net.minecraft.util.EnumParticleTypes;
@@ -14,11 +13,7 @@ public abstract class MixinNetHandlerPlayClient {
 	@Inject(method = "handleParticles", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketThreadUtil;checkThreadAndEnqueue(Lnet/minecraft/network/Packet;Lnet/minecraft/network/INetHandler;Lnet/minecraft/util/IThreadListener;)V", shift = At.Shift.AFTER))
 	private void himmelbjerget$onParticle(final S2APacketParticles p, final CallbackInfo ci) {
 		if (p.getParticleType() == EnumParticleTypes.EXPLOSION_LARGE) {
-			if (p.isLongDistance() && p.getParticleCount() == 8) {
-				((IS2APacketParticles) p).setParticleType(EnumParticleTypes.VILLAGER_HAPPY);
-			} else {
-				Himmelbjerget.LOGGER.info("{} {} {}", p.isLongDistance(), p.getParticleSpeed(), p.getParticleCount());
-			}
+			((IS2APacketParticles) p).setParticleType(EnumParticleTypes.VILLAGER_HAPPY);
 		}
 	}
 }

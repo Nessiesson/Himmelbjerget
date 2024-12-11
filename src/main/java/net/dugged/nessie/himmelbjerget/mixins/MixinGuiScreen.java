d323c6f8557cc871a110c5f07b59fef3ac3c7f35
nessie <nessie@hotmail.dk>
2024-12-11 14:17:23 +0100

    Log "hidden" commands in chat.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinGuiScreen.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinGuiScreen.java
@@ -1,7 +1,11 @@
 package net.dugged.nessie.himmelbjerget.mixins;
 
-import net.dugged.nessie.himmelbjerget.Himmelbjerget;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.event.ClickEvent;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +16,10 @@ public abstract class MixinGuiScreen {
 	@Inject(method = "sendChatMessage(Ljava/lang/String;Z)V", at = @At("HEAD"))
 	private void himmelbjerget$logHiddenCommands(final String msg, final boolean addToChat, final CallbackInfo ci) {
 		if (!addToChat) {
-			Himmelbjerget.LOGGER.info("Executed {}", msg);
+			final var text = new ChatComponentText("");
+			text.appendSibling(new ChatComponentText("Executed: ").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GREEN)));
+			text.appendSibling(new ChatComponentText(msg).setChatStyle(new ChatStyle().setColor(EnumChatFormatting.YELLOW).setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, msg))));
+			Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(text);
 		}
 	}
 }

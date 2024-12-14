f9d627d715c274508264a4948d5a2d298bb1c677
nessie <nessie@hotmail.dk>
2024-12-14 19:14:17 +0100

    Modify NEU's item cooldown animation to go down in discrete steps.

---
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinItemCooldowns.java
@@ -0,0 +1,20 @@
+package net.dugged.nessie.himmelbjerget.mixins;
+
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Pseudo
+@Mixin(targets = "io/github/moulberry/notenoughupdates/miscfeatures/ItemCooldowns")
+public class MixinItemCooldowns {
+	@Dynamic
+	@Inject(method = "getDurabilityOverride", remap = false, at = @At("RETURN"), cancellable = true)
+	private static void himmelbjerget$decrementInDiscreteSteps(final ItemStack stack, final CallbackInfoReturnable<Float> cir) {
+		final var factor = 1D / 8D;
+		cir.setReturnValue((float) (Math.ceil(cir.getReturnValue() / factor) * factor));
+	}
+}

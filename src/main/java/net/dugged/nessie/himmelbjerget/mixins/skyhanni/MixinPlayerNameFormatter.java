403292444f86603f9d92ac47982fa60ee7a94100
nessie <nessie@hotmail.dk>
2025-01-06 11:39:43 +0100

    Ensure that player messages are always white.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/skyhanni/MixinPlayerNameFormatter.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/skyhanni/MixinPlayerNameFormatter.java
@@ -2,6 +2,7 @@ package net.dugged.nessie.himmelbjerget.mixins.skyhanni;
 
 import net.dugged.nessie.himmelbjerget.IChatComponentText;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,11 +11,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
-@Mixin(targets = "at/hannibal2/skyhanni/data/hypixel/chat/PlayerNameFormatter")
+@Mixin(targets = "at/hannibal2/skyhanni/data/hypixel/chat/PlayerNameFormatter", remap = false)
 public abstract class MixinPlayerNameFormatter {
 	@Dynamic
-	@Redirect(method = "nameFormat", remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ChatComponentText;appendSibling(Lnet/minecraft/util/IChatComponent;)Lnet/minecraft/util/IChatComponent;", remap = true))
-	private IChatComponent himmelbjerget$test(final ChatComponentText instance, final IChatComponent component) {
+	@Redirect(method = "nameFormat", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ChatComponentText;appendSibling(Lnet/minecraft/util/IChatComponent;)Lnet/minecraft/util/IChatComponent;", remap = true))
+	private IChatComponent himmelbjerget$removeSpaceBeforeIronmanSymbol(final ChatComponentText instance, final IChatComponent component) {
 		if (component instanceof ChatComponentText) {
 			final var siblings = instance.getSiblings();
 			if ("§7♲".equals(((IChatComponentText) component).himmelbjerget$getText()) && !siblings.isEmpty()) {
@@ -23,6 +24,20 @@ public abstract class MixinPlayerNameFormatter {
 				if (last instanceof ChatComponentText && " ".equals(((IChatComponentText) last).himmelbjerget$getText())) {
 					siblings.remove(index);
 				}
+			}
+		}
+
+		return instance.appendSibling(component);
+	}
+
+	@Dynamic
+	@Redirect(method = "onPlayerAllChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ChatComponentText;appendSibling(Lnet/minecraft/util/IChatComponent;)Lnet/minecraft/util/IChatComponent;", ordinal = 2, remap = true))
+	private IChatComponent himmelbjerget$makeSameChatColorGreatAgain(final ChatComponentText instance, final IChatComponent component) {
+		final var siblings = component.getSiblings();
+		if (!siblings.isEmpty()) {
+			final var style = siblings.get(0).getChatStyle();
+			if (style.getColor() == EnumChatFormatting.GRAY) {
+				style.setColor(EnumChatFormatting.WHITE);
 			}
 		}
 

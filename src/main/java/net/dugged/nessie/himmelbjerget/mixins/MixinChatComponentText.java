0f33f817cb8feed9866af1d88a92c50ca0fc84f9
nessie <nessie@hotmail.dk>
2025-01-17 19:21:18 +0100

    debugging code idk.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinChatComponentText.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinChatComponentText.java
@@ -1,15 +1,17 @@
 package net.dugged.nessie.himmelbjerget.mixins;
 
 import net.dugged.nessie.himmelbjerget.IChatComponentText;
+import net.minecraft.util.ChatComponentStyle;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ChatComponentText.class)
-public abstract class MixinChatComponentText implements IChatComponent, IChatComponentText {
+public abstract class MixinChatComponentText extends ChatComponentStyle implements IChatComponent, IChatComponentText {
 	@Mutable
 	@Shadow
 	@Final
@@ -28,5 +30,14 @@ public abstract class MixinChatComponentText implements IChatComponent, IChatCom
 	@Override
 	public void himmelbjerget$setText(final String text) {
 		this.text = text;
+	}
+
+	/**
+	 * @author nessie
+	 * @reason easier debugging
+	 */
+	@Overwrite(remap = false)
+	public String toString() {
+		return "TC{text='" + this.text + '\'' + (!this.siblings.isEmpty() ? ", siblings=" + this.siblings : "") + '}';
 	}
 }

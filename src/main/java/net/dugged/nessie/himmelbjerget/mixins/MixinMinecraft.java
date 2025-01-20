b7495ac5a3065ae224c784509427909457dd52b4
nessie <nessie@hotmail.dk>
2025-01-20 11:54:53 +0100

    Remove F3+S redirect that is handled by Patcher.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinMinecraft.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinMinecraft.java
@@ -5,8 +5,6 @@ import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -15,9 +13,5 @@ public abstract class MixinMinecraft {
 	private void himmelbjerget$toggleFullScreen(final CallbackInfo ci) {
 		Display.setResizable(false);
 		Display.setResizable(true);
-	}
-
-	@Redirect(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;refreshResources()V", opcode = 0), slice = @Slice(from = @At(value = "CONSTANT", args = "intValue=31")))
-	private void himmelbjerget$noF3plusS(final Minecraft instance) {
 	}
 }

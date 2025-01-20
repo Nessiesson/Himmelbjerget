ecd8254128ae4e3fac5184bcd8bf84ead0654b18
nessie <nessie@hotmail.dk>
2025-01-20 13:20:46 +0100

    Steal weird culling method from glass block.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinBlockBarrier.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/MixinBlockBarrier.java
@@ -3,7 +3,12 @@ package net.dugged.nessie.himmelbjerget.mixins;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBarrier;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -25,5 +30,26 @@ public abstract class MixinBlockBarrier extends Block {
 	@Override
 	public EnumWorldBlockLayer getBlockLayer() {
 		return EnumWorldBlockLayer.TRANSLUCENT;
+	}
+
+	@Override
+	public boolean shouldSideBeRendered(final IBlockAccess world, final BlockPos pos, final EnumFacing side) {
+		final IBlockState state = world.getBlockState(pos);
+		final Block block = state.getBlock();
+		if (this == Blocks.barrier) {
+			if (world.getBlockState(pos.offset(side.getOpposite())) != state) {
+				return true;
+			}
+
+			if (block == this) {
+				return false;
+			}
+		}
+
+		if (block == this) {
+			return false;
+		}
+
+		return super.shouldSideBeRendered(world, pos, side);
 	}
 }

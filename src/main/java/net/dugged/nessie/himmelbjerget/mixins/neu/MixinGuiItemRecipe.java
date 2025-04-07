2ea54c0fb6810c9d2f8b3bdf4fca9bcf09b02818
nessie <nessie@hotmail.dk>
2025-04-07 23:37:15 +0200

    Move some enchanted things to the frost of recipe list to make garden crafting faster.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/neu/MixinGuiItemRecipe.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/neu/MixinGuiItemRecipe.java
@@ -18,7 +18,16 @@ public abstract class MixinGuiItemRecipe {
 	@Unique
 	private final static Comparator<NeuRecipe> himmelbjerget$cmpEnchFirst = (o1, o2) -> {
 		if (o1 == o2) return 0;
-		if (o1 instanceof CraftingRecipe c1 && c1.getOutput().getInternalItemId().startsWith("ENCHANTED")) return -1;
+		if (o1 instanceof CraftingRecipe c1) {
+			final var id = c1.getOutput().getInternalItemId();
+			if (id.startsWith("BOX_OF_SEEDS")) return -1;
+			if (id.startsWith("MUTANT_NETHER_STALK")) return -1;
+			if (id.startsWith("POLISHED_PUMPKIN")) return -1;
+			if (id.startsWith("ENCHANTED_COOKIE")) return -1;
+			if (id.startsWith("ENCHANTED_GOLDEN_CARROT")) return -1;
+			if (id.startsWith("ENCHANTED_SUGAR_CANE")) return -1;
+			if (id.startsWith("ENCHANTED")) return -1;
+		}
 		return 1;
 	};
 

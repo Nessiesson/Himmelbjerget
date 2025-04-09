391c47a547f7e3218908a29f4089b863169298e5
nessie <nessie@hotmail.dk>
2025-04-09 20:04:17 +0200

    Put some of the useful enchanted items in front of the not to useful enchanted items in the recipe list.

--- src/main/java/net/dugged/nessie/himmelbjerget/mixins/neu/MixinGuiItemRecipe.java
+++ src/main/java/net/dugged/nessie/himmelbjerget/mixins/neu/MixinGuiItemRecipe.java
@@ -16,26 +16,27 @@ import java.util.List;
 @Mixin(targets = "io/github/moulberry/notenoughupdates/miscgui/GuiItemRecipe", remap = false)
 public abstract class MixinGuiItemRecipe {
 	@Unique
-	private final static Comparator<NeuRecipe> himmelbjerget$cmpEnchFirst = (o1, o2) -> {
-		if (o1 == o2) return 0;
-		if (o1 instanceof CraftingRecipe c1) {
-			final var id = c1.getOutput().getInternalItemId();
-			if (id.startsWith("BOX_OF_SEEDS")) return -1;
-			if (id.startsWith("MUTANT_NETHER_STALK")) return -1;
-			if (id.startsWith("POLISHED_PUMPKIN")) return -1;
-			if (id.startsWith("ENCHANTED_COOKIE")) return -1;
-			if (id.startsWith("ENCHANTED_GOLDEN_CARROT")) return -1;
-			if (id.startsWith("ENCHANTED_SUGAR_CANE")) return -1;
-			if (id.startsWith("ENCHANTED_WHEAT")) return -1;
-			if (id.startsWith("ENCHANTED")) return -1;
+	private static int himmelbjerget$getPriority(final NeuRecipe recipe) {
+		if (recipe instanceof final CraftingRecipe craft) {
+			final var id = craft.getOutput().getInternalItemId();
+			if (id.equals("BOX_OF_SEEDS")) return 0;
+			if (id.equals("MUTANT_NETHER_STALK")) return 0;
+			if (id.equals("POLISHED_PUMPKIN")) return 0;
+			if (id.equals("ENCHANTED_COOKIE")) return 0;
+			if (id.equals("ENCHANTED_GOLDEN_CARROT")) return 0;
+			if (id.equals("ENCHANTED_SUGAR_CANE")) return 0;
+			if (id.equals("ENCHANTED_WHEAT")) return 0;
+
+			if (id.startsWith("ENCHANTED_")) return 1;
 		}
-		return 1;
-	};
+
+		return 2;
+	}
 
 	@Dynamic
 	@ModifyVariable(method = "<init>(Ljava/util/List;Lio/github/moulberry/notenoughupdates/NEUManager;)V", at = @At("HEAD"), argsOnly = true)
 	private static List<NeuRecipe> himmelbjerget$putEnchangedItemsFirst(final List<NeuRecipe> unsortedRecipes) {
-		unsortedRecipes.sort(himmelbjerget$cmpEnchFirst);
+		unsortedRecipes.sort(Comparator.comparingInt(MixinGuiItemRecipe::himmelbjerget$getPriority));
 		return unsortedRecipes;
 	}
 }
